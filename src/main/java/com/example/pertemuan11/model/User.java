7e55ee76cca8651acf69f2e79aae3cbab2b66eea
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-05-19 09:52:52 +0800

    Menambahkan Isi untuk User.java

--- src/main/java/com/example/pertemuan11/model/User.java
+++ src/main/java/com/example/pertemuan11/model/User.java
@@ -1,4 +1,24 @@
 package com.example.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.swing.*;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private Spring password;
+    @OneToOne(mappedBy = "users", cascade = CascadeType.ALL)
+    private Profile profile;
 }
